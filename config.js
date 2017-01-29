cdf89e68402269b24a79c38a6232878c39df5ea5
Gagan Madan <gagan7535@gmail.com>
2017-01-30 03:09:07 +0530

    config changes

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    "database" : "mongodb://GaganMadan:gagan123@ds059306.mlab.com:59306/rest_test",
+    "port": process.env.PORT || 3000,
+    "secretKey" : "YourSecretKey"
+}
